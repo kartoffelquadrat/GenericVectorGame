bf69733fed0120a57960c44ea198cc682ef72bec
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2022-07-15 16:04:02 -0400

    Added link / script call to SVG patcher

--- scripts/svgpatch.sh
+++ scripts/svgpatch.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 ## SVG Patch script - M.Schiedermeier, 2022
 ## maximilian.schiedermeier@mcgill.ca
-##
-## This script patches SVG files to apply object IDs set via omnigraffle as object IDs.
-## If in OmniGraffle an object, e.g. a rectangle was provided with "Object Data Name": "ID-FOO", then exported. This string is not yet used as object id in the svg representation.
-## Running this script ensures all id strings set with omnigraffle are then likewise copied for the corresponding object id.
-## This allows a subsequent selection and manipulation of these objects via JS DOM operations.
+## Literally just a call to the JAR file compiled form this repo:
+## https://github.com/kartoffelquadrat/SvgPatcher
+#
+java -Djavax.xml.accessExternalDTD=all -jar ~/Code/SvgPatcher/target/svgpatcher.jar vectorBoard.svg patchedVectorBoard.svg
+
