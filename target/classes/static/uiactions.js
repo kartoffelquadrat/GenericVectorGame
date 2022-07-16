65cff09eb9a97be208006f2e90d2b99028c92412
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2022-07-16 17:39:20 -0400

    fixed JS based SVG manipulation

--- target/classes/static/uiactions.js
+++ target/classes/static/uiactions.js
@@ -3,21 +3,27 @@ function showAlert() {
     alert("yay");
 }
 
-
-function registerHandlers() {
-
+function getSquareElement() {
     // https://www.petercollingridge.co.uk/tutorials/svg/interactive/javascript/
     let svg = document.getElementById('board');
     console.log(svg);
-    let element = svg.getElementById('VID-SQUARE1');
-    console.log(element);
+    let aRectangleWithIdSquare1 = svg.contentDocument.getElementById('VID-SQUARE1');
+    console.log(aRectangleWithIdSquare1);
+    return aRectangleWithIdSquare1.childNodes[1]; // TODO: use better way to find rect with fill.
+}
+
+
+function registerHandlers() {
+
+    console.log("DOM tree is ready");
+    let rect = getSquareElement();
+    rect.setAttribute('fill', '#fff000');
 
 }
 
 
 function registerHandlersWhenSvgLoaded() {
 
-    // $(document).ready(registerHandlers);
-
-
+    console.log("Waiting for DOM tree to be ready...");
+    $(document).ready(registerHandlers);
 }
