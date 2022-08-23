0f8bac73799c6166682640c41be7fb3a1fd0f9d9
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2022-08-23 15:38:18 -0400

    added todos

--- src/main/resources/static/asyncuiactions.js
+++ src/main/resources/static/asyncuiactions.js
@@ -3,10 +3,7 @@ function updateDisplayedAnimalName(stateBundle) {
     console.log("updating animal name to: " + stateBundle.animal);
     // Todo: actually change the name in the SVG dom tree.
     let gcircle = getSvgElementById('VID-CIRCLE');
-    console.log(gcircle);
-    console.log("Changing text of "+gcircle+" to "+stateBundle.animal);
     let text = gcircle.children[2]; //use children, not childNodes! https://stackoverflow.com/a/27501373
     let tspan = text.children[0];
-    console.log(tspan);
-    tspan.textContent=stateBundle.animal;
+    tspan.textContent=stateBundle.animal; // Todo: add text-anchor="middle" as attribute to all tspans. Consider doing that in svg transformer.
 }
